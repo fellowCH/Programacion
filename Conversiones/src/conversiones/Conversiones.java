febfc2a6cbae00a9a8b517abb41162fcc7761e50
Ricardo Ortez <Ricardo Ortez@Richard>
2026-07-27 20:52:57 -0600

    HellowWorld 24/7/26 Aprendimos prints y demas utilidades

---
+++ Conversiones/src/conversiones/Conversiones.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package conversiones;
+import java.util.Scanner;
+/**
+ *
+ * @author Ricardo Ortez
+ */
+public class Conversiones {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Scanner teclado = new Scanner(System.in);
+        
+        int pulgadas = 0;
+        double resultadoCM = 0;
+        
+        double kilometros = 0;
+        double millas = 0;
+        
+        double farenheit = 0;
+        double celsius = 0;
+        
+        pulgadas = 10;  
+        resultadoCM = pulgadas * 2.54;
+        System.out.printf("%d pulgadas es igual a %.2f cm", pulgadas, resultadoCM);
+        
+        pulgadas = 35;
+        resultadoCM = pulgadas * 2.54;
+        System.out.printf("\n%.2f cm es igual a %d pulgadas", resultadoCM, pulgadas);
+        
+        System.out.println("");
+        System.out.println("Cuantos kilometros hay de TGU - SPS");
+        kilometros = teclado.nextDouble();
+        millas = kilometros / 1.6;
+        System.out.printf("%.2f km es igual a %.2f millas \n", kilometros, millas);
+        System.out.println(kilometros + " km es igual a " + millas + "millas \n");
+        
+        
+        System.out.println("Ingresa las temperaturas en farenheit. \n");
+        farenheit = teclado.nextDouble();
+        celsius = (farenheit - 32) * 5/9;
+        System.out.printf("%.2f Farenheit equivale a %.2f celsius.", farenheit, celsius);
+        
+        
+    }
+    
+}
